dead684ec8a227f6a6ccdb82cb498d75d8fa41b2
longbiu <longbiu@foxmail.com>
2025-10-30 13:39:42 +0800

    fix readme

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 from https://docs.djangoproject.com/zh-hans/5.2/intro/
+
 一个上手项目
--- mysite/urls.py
+++ mysite/urls.py
@@ -3,5 +3,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path("polls/", include("polls.urls")),
+    path("grappelli/", include("grappelli.urls")),
     path("admin/", admin.site.urls),
 ]
